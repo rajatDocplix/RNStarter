1211c8ff49ab7f1510e29d14ea63c9b9ac6aaa01
Rajat Bhati <129043111+rajatDocplix@users.noreply.github.com>
2023-08-21 18:30:54 +0530

    1st commit

--- ios/RNStarterTests/RNStarterTests.m
+++ ios/RNStarterTests/RNStarterTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnstarterTests : XCTestCase
+@interface RNstarterTests : XCTestCase
 
 @end
 
-@implementation rnstarterTests
+@implementation RNstarterTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
